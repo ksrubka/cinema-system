edb44e61e895077de4c7822293e71b4845f0ea81
Nizari <nizari@o2.pl>
2016-09-04 15:47:24 +0200

    create method

--- src/main/java/pl/com/bottega/cinemasystem/ui/CinemasController.java
+++ src/main/java/pl/com/bottega/cinemasystem/ui/CinemasController.java
@@ -1,13 +1,20 @@
 package pl.com.bottega.cinemasystem.ui;
 
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.com.bottega.cinemasystem.api.AdminPanel;
 import pl.com.bottega.cinemasystem.api.CreateCinemaRequest;
 
+@RestController
+@RequestMapping("/cinemas")
 public class CinemasController {
 
     private AdminPanel adminPanel;
 
-    public void create(CreateCinemaRequest createCinemaRequest) {
+    @PutMapping
+    public void create(@RequestBody CreateCinemaRequest createCinemaRequest) {
         adminPanel.createCinema(createCinemaRequest);
     }
 }
