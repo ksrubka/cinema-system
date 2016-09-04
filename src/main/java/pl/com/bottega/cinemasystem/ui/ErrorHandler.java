afd87308709219bd73ed1954caeffa02c6ef861b
diack <you@example.com>
2016-09-04 13:49:29 +0200

    Error handler update

--- src/main/java/pl/com/bottega/cinemasystem/ui/ErrorHandler.java
+++ src/main/java/pl/com/bottega/cinemasystem/ui/ErrorHandler.java
@@ -5,20 +5,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import pl.com.bottega.cinemasystem.api.InvalidRequestException;
 
 @ControllerAdvice
 public class ErrorHandler {
 
-
     @ExceptionHandler(InvalidRequestException.class)
-    public ResponseEntity<String> handleAuthRequiredException() {
+    public ResponseEntity<String> handleAuthRequiredException(MultipartHttpServletRequest req, InvalidRequestException ex) {
         HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
+        headers.set(HttpHeaders.CONTENT_TYPE, "application/json"); //TODO
+        String msg = "{'error': '%s'}";
         return new ResponseEntity<String>(
-                "{'error': 'Invalid Request Exception'}",
+                String.format(msg, ex.getMessage()),
                 headers,
                 HttpStatus.UNPROCESSABLE_ENTITY
         );
     }
+
 }
