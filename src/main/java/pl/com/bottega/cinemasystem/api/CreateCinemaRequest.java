dfd74def92b5da9f0e533f92c8c5603912c59363
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-09-04 13:01:49 +0200

    fields, constructor added

--- src/main/java/pl/com/bottega/cinemasystem/api/CreateCinemaRequest.java
+++ src/main/java/pl/com/bottega/cinemasystem/api/CreateCinemaRequest.java
@@ -1,4 +1,20 @@
 package pl.com.bottega.cinemasystem.api;
 
+import pl.com.bottega.cinemasystem.domain.CinemaRepository;
+
 public class CreateCinemaRequest {
+
+    private String name;
+    private String city;
+
+
+    public CreateCinemaRequest(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+
+     public void validate (CinemaRepository cinemaRepository){
+
+     }
 }
