3d9c2e83375cde3d2def14755809e7331c4f5ccc
ksrubka <beata.choluj@wp.pl>
2016-09-04 13:48:51 +0200

    Boy scout ;)

--- src/main/java/pl/com/bottega/cinemasystem/api/InvalidRequestException.java
+++ src/main/java/pl/com/bottega/cinemasystem/api/InvalidRequestException.java
@@ -3,7 +3,6 @@ package pl.com.bottega.cinemasystem.api;
 public class InvalidRequestException extends RuntimeException {
 
     public InvalidRequestException(String msg) {
-
         super(msg);
     }
 }
