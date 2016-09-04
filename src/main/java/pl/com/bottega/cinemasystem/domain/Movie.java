4929c33cd81b4ee0986283e54f0e98467f08707c
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-09-04 11:46:34 +0200

    getters & setters added

--- src/main/java/pl/com/bottega/cinemasystem/domain/Movie.java
+++ src/main/java/pl/com/bottega/cinemasystem/domain/Movie.java
@@ -5,5 +5,16 @@ package pl.com.bottega.cinemasystem.domain;
  */
 public class Movie {
     String title;
-    
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+
+
 }
