cbae380810fe292befb7a5484c633ce534655d68
ksrubka <beata.choluj@wp.pl>
2016-09-04 11:50:53 +0200

    Add Movie constructor

--- src/main/java/pl/com/bottega/cinemasystem/domain/Movie.java
+++ src/main/java/pl/com/bottega/cinemasystem/domain/Movie.java
@@ -4,6 +4,10 @@ package pl.com.bottega.cinemasystem.domain;
  * Created by paulina.pislewicz on 2016-09-04.
  */
 public class Movie {
-    String title;
-    
+
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
 }
