b3ebdfeba5ae33322718cd65244baa71accf5412
ksrubka <beata.choluj@wp.pl>
2016-09-04 14:20:35 +0200

    Implement CinemaDto

--- src/main/java/pl/com/bottega/cinemasystem/api/CinemaDto.java
+++ src/main/java/pl/com/bottega/cinemasystem/api/CinemaDto.java
@@ -1,4 +1,35 @@
 package pl.com.bottega.cinemasystem.api;
 
+import pl.com.bottega.cinemasystem.domain.CinemaRepository;
+
 public class CinemaDto {
+
+    private String name;
+    private String city;
+
+    public void validate (CinemaRepository cinemaRepository){
+        if (this.name == null || this.name.trim().isEmpty())
+            throw new InvalidRequestException("The cinema's name is required");
+        if (this.city == null || this.city.trim().isEmpty())
+            throw new InvalidRequestException("The city is required");
+
+        if (cinemaRepository.load(this.name, this.city)!= null)
+            throw new InvalidRequestException("This cinema exists in the system");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
