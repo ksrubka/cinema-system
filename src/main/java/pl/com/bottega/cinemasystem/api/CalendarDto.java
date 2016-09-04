09da2b2f989be4a87ac68635d2ad69699afcfa9a
Nizari <nizari@o2.pl>
2016-09-04 12:32:37 +0200

    Classes

--- src/main/java/pl/com/bottega/cinemasystem/api/CalendarDto.java
+++ src/main/java/pl/com/bottega/cinemasystem/api/CalendarDto.java
@@ -1,7 +1,13 @@
 package pl.com.bottega.cinemasystem.api;
 
+import java.util.Date;
+
 /**
  * Created by Nizari on 04.09.2016.
  */
 public class CalendarDto {
+
+    private Date fromDate;
+    private Date untilDate;
+
 }
