5d3d99a3beca5f4b9cb3304e53bd3505bf421115
ksrubka <beata.choluj@wp.pl>
2016-09-06 16:16:57 +0200

    Implement CreateShowsRequest

--- src/main/java/pl/com/bottega/cinemasystem/api/CreateShowsRequest.java
+++ src/main/java/pl/com/bottega/cinemasystem/api/CreateShowsRequest.java
@@ -1,5 +1,29 @@
 package pl.com.bottega.cinemasystem.api;
 
+import java.util.Date;
+import java.util.List;
 
 public class CreateShowsRequest {
+
+    private ShowsWrapper shows;
+
+    public void validate() {
+        shows.validate();
+    }
+
+    public Long getMovieId() {
+        return shows.getMovieId();
+    }
+
+    public List<Date> getShowDates() {
+        return shows.getShowDates();
+    }
+
+    public ShowsWrapper getShows() {
+        return shows;
+    }
+
+    public void setShows(ShowsWrapper shows) {
+        this.shows = shows;
+    }
 }
