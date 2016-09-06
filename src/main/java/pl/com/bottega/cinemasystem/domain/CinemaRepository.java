46be15e3936090f21d3696f8c0ec8d7de607eccc
ksrubka <beata.choluj@wp.pl>
2016-09-06 15:22:33 +0200

    Add load (by id) method signature to CinemaRepo

--- src/main/java/pl/com/bottega/cinemasystem/domain/CinemaRepository.java
+++ src/main/java/pl/com/bottega/cinemasystem/domain/CinemaRepository.java
@@ -3,5 +3,5 @@ package pl.com.bottega.cinemasystem.domain;
 public interface CinemaRepository {
 
     void save(Cinema cinema);
-    Cinema load(String name, String city);
+    Cinema load(Long id);
 }
