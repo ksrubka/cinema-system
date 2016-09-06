ac89e7757337d6d1afd4dd6dc84bd46cc87523c8
ksrubka <beata.choluj@wp.pl>
2016-09-06 15:57:17 +0200

    Add fields, getters and setters to CalendarDto

--- src/main/java/pl/com/bottega/cinemasystem/api/CalendarDto.java
+++ src/main/java/pl/com/bottega/cinemasystem/api/CalendarDto.java
@@ -1,10 +1,44 @@
 package pl.com.bottega.cinemasystem.api;
 
+import java.util.Collection;
 import java.util.Date;
 
 public class CalendarDto {
 
     private Date fromDate;
     private Date untilDate;
+    private Collection<Weekday> weekDays;
+    private Collection<String> hours;
 
+    public Date getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(Date fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public Date getUntilDate() {
+        return untilDate;
+    }
+
+    public void setUntilDate(Date untilDate) {
+        this.untilDate = untilDate;
+    }
+
+    public Collection<Weekday> getWeekDays() {
+        return weekDays;
+    }
+
+    public void setWeekDays(Collection<Weekday> weekDays) {
+        this.weekDays = weekDays;
+    }
+
+    public Collection<String> getHours() {
+        return hours;
+    }
+
+    public void setHours(Collection<String> hours) {
+        this.hours = hours;
+    }
 }
