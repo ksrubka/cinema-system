ff557fd8f1c374be0dc03b6906fd701458cf5acf
ksrubka <beata.choluj@wp.pl>
2016-09-06 08:27:13 +0200

    Remove unused param from handleInvalidRequestException method in ErrorHandler

--- src/main/java/pl/com/bottega/cinemasystem/ui/ErrorHandler.java
+++ src/main/java/pl/com/bottega/cinemasystem/ui/ErrorHandler.java
@@ -12,15 +12,15 @@ import pl.com.bottega.cinemasystem.api.InvalidRequestException;
 public class ErrorHandler {
 
     @ExceptionHandler(InvalidRequestException.class)
-    public ResponseEntity<String> handleAuthRequiredException(MultipartHttpServletRequest req, InvalidRequestException ex) {
+    public ResponseEntity<String> handleInvalidRequestException(InvalidRequestException ex) {
         HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_TYPE, "application/json"); //TODO
+        headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
         String msg = "{'error': '%s'}";
         return new ResponseEntity<String>(
                 String.format(msg, ex.getMessage()),
                 headers,
                 HttpStatus.UNPROCESSABLE_ENTITY
-        );//jdbc:hsqldb:hsql://localhost:9001/cinema
+        );
     }
 
 }
