a429f33fec37c9afdc7a4a2a478303f421d4b61a
ksrubka <beata.choluj@wp.pl>
2016-09-09 22:43:16 +0200

    Add validation and generating show dates to ShowDto

--- src/main/java/pl/com/bottega/cinemasystem/api/ShowsDto.java
+++ src/main/java/pl/com/bottega/cinemasystem/api/ShowsDto.java
@@ -1,19 +1,17 @@
 package pl.com.bottega.cinemasystem.api;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
-//todo decide polymorphism or logic
 public class ShowsDto {
 
     private Long movieId;
     private CalendarDto calendar;
-    private Collection<Date> dates;
+    private Collection<String> dates;
 
     public List<Date> getShowDates() {
-        return new ArrayList<>(); //TODO add logic
+        return generateStrategyCreatingShowDates().generateShowDates();
     }
 
     public Long getMovieId() {
@@ -32,15 +30,31 @@ public class ShowsDto {
         this.calendar = calendar;
     }
 
-    public Collection<Date> getDates() {
+    public Collection<String> getDates() {
         return dates;
     }
 
-    public void setDates(Collection<Date> dates) {
+    public void setDates(Collection<String> dates) {
         this.dates = dates;
     }
 
     public void validate() {
-    //todo validate
+        checkMovieId();
+        generateStrategyCreatingShowDates().validate();
+    }
+
+    private void checkMovieId() {
+        if (movieId < 0) {
+            throw new InvalidRequestException("Incorrect movie id");
+        }
+    }
+
+    private DatesCreatingStrategy generateStrategyCreatingShowDates() {
+        if (calendar == null) {
+            return new StringsBasedDatesCreatingStrategy(dates);
+        }
+        else {
+            return new CalendarBasedDatesCreatingStrategy(calendar);
+        }
     }
 }
