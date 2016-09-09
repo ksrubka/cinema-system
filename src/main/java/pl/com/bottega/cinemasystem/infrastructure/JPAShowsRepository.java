4d5309e5ca1b47de3d2328573669209c204f443e
ksrubka <beata.choluj@wp.pl>
2016-09-09 12:43:48 +0200

    Add save method to JPAShowsRepo

--- src/main/java/pl/com/bottega/cinemasystem/infrastructure/JPAShowsRepository.java
+++ src/main/java/pl/com/bottega/cinemasystem/infrastructure/JPAShowsRepository.java
@@ -1,4 +1,21 @@
 package pl.com.bottega.cinemasystem.infrastructure;
 
-public class JPAShowsRepository {
+import org.springframework.stereotype.Repository;
+import pl.com.bottega.cinemasystem.domain.Show;
+import pl.com.bottega.cinemasystem.domain.ShowsRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class JPAShowsRepository implements ShowsRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Override
+    public void save(Show show) {
+        entityManager.persist(show);
+    }
 }
