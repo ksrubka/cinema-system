26373ff0c4a9569ff2c18786609a01c72e2e1987
ksrubka <beata.choluj@wp.pl>
2016-09-10 14:42:02 +0200

    Remove static modifier from MovieFactory create

--- src/main/java/pl/com/bottega/cinemasystem/api/MovieFactory.java
+++ src/main/java/pl/com/bottega/cinemasystem/api/MovieFactory.java
@@ -2,10 +2,11 @@ package pl.com.bottega.cinemasystem.api;
 
 import org.springframework.stereotype.Service;
 import pl.com.bottega.cinemasystem.domain.Movie;
+
 @Service
 public class MovieFactory {
 
-    public static Movie createMovie(CreateMovieRequest request) {
+    public Movie createMovie(CreateMovieRequest request) {
         return new Movie (request.getTitle(), request.getDescription(), request.getMinAge(), request.getActors(),
                 request.getGenres(), request.getLength());
     }
