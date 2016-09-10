8532e36542ee13de7184207702ffc6d8d4ac258a
Nizari <nizari@o2.pl>
2016-09-10 12:10:41 +0200

    Remake of Movie

--- src/main/java/pl/com/bottega/cinemasystem/domain/Movie.java
+++ src/main/java/pl/com/bottega/cinemasystem/domain/Movie.java
@@ -32,4 +32,60 @@ public class Movie {
         this.genres = genres;
         this.length = length;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getMinAge() {
+        return minAge;
+    }
+
+    public void setMinAge(Integer minAge) {
+        this.minAge = minAge;
+    }
+
+    public List<String> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<String> actors) {
+        this.actors = actors;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
 }
