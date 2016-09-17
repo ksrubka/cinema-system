b9cfef8dde72aface0aaa56913315e5b0c01b660
wojtek <diackon@interia.pl>
2016-09-17 09:27:25 +0200

    Create Show Test

--- src/test/java/pl/com/bottega/cinemasystem/api/CreateShowsRequestTest.java
+++ src/test/java/pl/com/bottega/cinemasystem/api/CreateShowsRequestTest.java
@@ -1,4 +1,48 @@
 package pl.com.bottega.cinemasystem.api;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.com.bottega.cinemasystem.domain.ShowsRepository;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class CreateShowsRequestTest {
+
+
+    private Long movieId = 15L;
+    private CalendarDto calendar;
+    private Collection<String> dates = new ArrayList<>();
+    private CreateShowsRequest createShowsRequest;
+    @Mock
+    private ShowsRepository showsRepository;
+    @Mock
+    private ShowsDto showsDto;
+
+    @Before
+    public void setUp(){
+        createShowsRequest = new CreateShowsRequest();
+        dates.add("2017/09/01 14:00");
+    }
+    @Test
+    public void shouldValidateShow() {
+        //given
+        when(showsDto.getCalendar()).thenReturn(calendar);
+        when(showsDto.getDates()).thenReturn(dates);
+        when(showsDto.getMovieId()).thenReturn(movieId);
+        createShowsRequest.setShows(showsDto);
+        //when
+        createShowsRequest.validate();
+        //then
+        assertEquals(showsDto, createShowsRequest.getShows());
+
+    }
 }
