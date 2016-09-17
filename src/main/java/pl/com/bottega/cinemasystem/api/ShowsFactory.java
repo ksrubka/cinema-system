774c47bb971f6291c3ed395fec9c1dd9f6d0b0f1
Nizari <nizari@o2.pl>
2016-09-17 16:22:11 +0200

    Move code to ShowsFactory

---
+++ src/main/java/pl/com/bottega/cinemasystem/api/ShowsFactory.java
@@ -0,0 +1,24 @@
+package pl.com.bottega.cinemasystem.api;
+
+import org.springframework.stereotype.Component;
+import pl.com.bottega.cinemasystem.domain.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class ShowsFactory {
+
+    private CinemaRepository cinemaRepository;
+    private MovieRepository movieRepository;
+
+    public List<Show> getShows(Long cinemaId, CreateShowsRequest createShowsRequest) {
+        Cinema cinema = cinemaRepository.load(cinemaId);
+        Movie movie = movieRepository.load(createShowsRequest.getMovieId());
+        List<Date> dates = createShowsRequest.getShowDates();
+        List<Show> shows = new ArrayList<>();
+        dates.forEach(date -> shows.add(new Show(cinema, movie, date)));
+        return shows;
+    }
+}
