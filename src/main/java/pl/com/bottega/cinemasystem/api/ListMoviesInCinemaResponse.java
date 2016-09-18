7588dc6dc79c837b9df87913080273bf78aa96c7
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-09-18 08:14:48 +0200

    add class ListMoviesInCinemaResponse which contains collection of movies

---
+++ src/main/java/pl/com/bottega/cinemasystem/api/ListMoviesInCinemaResponse.java
@@ -0,0 +1,10 @@
+package pl.com.bottega.cinemasystem.api;
+
+import java.util.Collection;
+
+/**
+ * Created by paulina.pislewicz on 2016-09-17.
+ */
+public class ListMoviesInCinemaResponse {
+    Collection <MovieDtoWithShows> movies; //potrzebny osobną klasę na wzór movieDto z inną nazwą lub klasę wewnętrzną w tej klasie
+}
