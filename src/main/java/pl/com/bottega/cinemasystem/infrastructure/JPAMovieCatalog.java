975b457e71f8e83cf19ea0ee5767d3e083084add
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-09-18 09:13:56 +0200

    add annotation & persistence contex

--- src/main/java/pl/com/bottega/cinemasystem/infrastructure/JPAMovieCatalog.java
+++ src/main/java/pl/com/bottega/cinemasystem/infrastructure/JPAMovieCatalog.java
@@ -1,16 +1,31 @@
 package pl.com.bottega.cinemasystem.infrastructure;
 
+import org.springframework.stereotype.Repository;
 import pl.com.bottega.cinemasystem.api.ListMoviesInCinemaResponse;
 import pl.com.bottega.cinemasystem.api.MovieCatalog;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalTime;
 
 /**
  * Created by paulina.pislewicz on 2016-09-17.
  */
+
+@Repository
 public class JPAMovieCatalog implements MovieCatalog {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public ListMoviesInCinemaResponse listMoviesInCinema(Long cinemaId, LocalTime date) {
-        return null;
+        /*String jpa = "select new pl.com.bottega.cinemasystem.api.MovieDtoWithShows(" +
+                "m.id, m.name, c.city) FROM Movie m";
+        Query query = entityManager.createQuery(jpa, CinemaDto.class);
+        ListMoviesInCinemaResponse moviesList = new ListMoviesInCinemaResponse(query.getResultList());
+        return moviesList;*/
+    return null;
     }
+
 }
