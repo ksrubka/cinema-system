1965fe902248a554e5b9a1981793de726c513b68
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-09-18 13:10:34 +0200

    change LocalTime into LocalDate

--- src/main/java/pl/com/bottega/cinemasystem/api/MovieCatalog.java
+++ src/main/java/pl/com/bottega/cinemasystem/api/MovieCatalog.java
@@ -1,10 +1,10 @@
 package pl.com.bottega.cinemasystem.api;
 
-import java.time.LocalTime;
+import java.time.LocalDate;
 
 /**
  * Created by paulina.pislewicz on 2016-09-17.
  */
 public interface MovieCatalog {
-    ListMoviesInCinemaResponse listMoviesInCinema(Long cinemaId, LocalTime date);
+    ListMoviesInCinemaResponse listMoviesInCinema(Long cinemaId, LocalDate date);
 }
