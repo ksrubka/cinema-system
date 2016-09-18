076dc349d6eb470d3d42d056e55cb7ce084315e8
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-09-18 08:12:47 +0200

    add class (new version of MovieDto)

---
+++ src/main/java/pl/com/bottega/cinemasystem/api/MovieDtoWithShows.java
@@ -0,0 +1,14 @@
+package pl.com.bottega.cinemasystem.api;
+
+import java.util.Collection;
+
+/**
+ * Created by paulina.pislewicz on 2016-09-17.
+ */
+public class MovieDtoWithShows {
+    public String title;
+    public String description;
+    Collection<ShowDto> shows;
+
+
+}
