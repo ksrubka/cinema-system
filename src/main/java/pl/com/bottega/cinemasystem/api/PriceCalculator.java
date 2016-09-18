6b1e41f341b6a121929b34821f0a55e0a424d276
diack <you@example.com>
2016-09-18 16:43:26 +0200

    PriceCalculator fields

--- src/main/java/pl/com/bottega/cinemasystem/api/PriceCalculator.java
+++ src/main/java/pl/com/bottega/cinemasystem/api/PriceCalculator.java
@@ -2,9 +2,14 @@ package pl.com.bottega.cinemasystem.api;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.com.bottega.cinemasystem.domain.Show;
+import pl.com.bottega.cinemasystem.domain.ShowsRepository;
 
 @Service
 public class PriceCalculator {
+
+    private ShowsRepository showsRepository;
+    private Show show;
 
     @Transactional
     public CalculatePriceResponse calculatePrice(CalculatePriceRequest request) {
