8c2960219222ebc2590b5b0544118fe031e97f4f
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-09-18 13:09:48 +0200

    add constructor ListMoviesInCinemaResponse

--- src/main/java/pl/com/bottega/cinemasystem/api/ListMoviesInCinemaResponse.java
+++ src/main/java/pl/com/bottega/cinemasystem/api/ListMoviesInCinemaResponse.java
@@ -1,14 +1,25 @@
 package pl.com.bottega.cinemasystem.api;
 
+import pl.com.bottega.cinemasystem.domain.Movie;
+
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Created by paulina.pislewicz on 2016-09-17.
  */
 public class ListMoviesInCinemaResponse {
+    public ListMoviesInCinemaResponse(Collection<Movie> moviesCollection) {
+        this.movies = new HashSet<>();
+        for (Movie movie: moviesCollection){
+            this.movies.add(new MovieDtoWithShows(movie));
+        }
+    }
+
     public Collection<MovieDtoWithShows> getMovies() {
         return movies;
     }
 
-    Collection <MovieDtoWithShows> movies; //potrzebny osobną klasę na wzór movieDto z inną nazwą lub klasę wewnętrzną w tej klasie
+    Collection <MovieDtoWithShows> movies;
+
 }
