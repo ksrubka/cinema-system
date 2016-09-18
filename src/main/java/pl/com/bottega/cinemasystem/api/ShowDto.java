4578aa715ccf1537ca2230f34d0b5a5f75aa4e37
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-09-18 13:12:47 +0200

    add constructor and getters, make fields private

--- src/main/java/pl/com/bottega/cinemasystem/api/ShowDto.java
+++ src/main/java/pl/com/bottega/cinemasystem/api/ShowDto.java
@@ -1,6 +1,7 @@
 package pl.com.bottega.cinemasystem.api;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import pl.com.bottega.cinemasystem.domain.Show;
 
 import java.time.LocalTime;
 
@@ -8,7 +9,33 @@ import java.time.LocalTime;
  * Created by paulina.pislewicz on 2016-09-17.
  */
 public class ShowDto {
-    Long id;
-    @JsonFormat (pattern = "YYYY/MM/dd")
-    LocalTime time;
+    private Long id;
+    @JsonFormat (pattern = "HH:mm")
+    private LocalTime time;
+
+    public ShowDto(Long id, LocalTime time) {
+        this.id = id;
+        this.time = time;
+    }
+
+    public ShowDto(Show show) {
+        this.id = show.getId();
+        this.time = show.getDate().toLocalTime();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
 }
