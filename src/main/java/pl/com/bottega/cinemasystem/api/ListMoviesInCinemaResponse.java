252e862f2d26a2ee2377e9cb31bbb445075bf1ee
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-09-18 09:15:50 +0200

    add getter tocollection of movieDtoWithShows

--- src/main/java/pl/com/bottega/cinemasystem/api/ListMoviesInCinemaResponse.java
+++ src/main/java/pl/com/bottega/cinemasystem/api/ListMoviesInCinemaResponse.java
@@ -6,5 +6,9 @@ import java.util.Collection;
  * Created by paulina.pislewicz on 2016-09-17.
  */
 public class ListMoviesInCinemaResponse {
+    public Collection<MovieDtoWithShows> getMovies() {
+        return movies;
+    }
+
     Collection <MovieDtoWithShows> movies; //potrzebny osobną klasę na wzór movieDto z inną nazwą lub klasę wewnętrzną w tej klasie
 }
