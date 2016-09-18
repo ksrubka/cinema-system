1ee6171068175b687899fcd88a4e53b10ba160e4
diack <you@example.com>
2016-09-18 11:47:24 +0200

    Calculation classes

---
+++ src/main/java/pl/com/bottega/cinemasystem/api/PriceCalculator.java
@@ -0,0 +1,11 @@
+package pl.com.bottega.cinemasystem.api;
+
+import pl.com.bottega.cinemasystem.domain.Show;
+import pl.com.bottega.cinemasystem.domain.ShowsRepository;
+
+public class PriceCalculator implements ShowsRepository {
+    @Override
+    public void save(Show show) {
+
+    }
+}
