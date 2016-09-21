60556d3fc0a125ae1134417e6dae18c109c23975
ksrubka <beata.choluj@wp.pl>
2016-09-21 15:47:14 +0200

    Change factory methods to static

--- src/main/java/pl/com/bottega/cinemasystem/domain/ShowsFactory.java
+++ src/main/java/pl/com/bottega/cinemasystem/domain/ShowsFactory.java
@@ -1,6 +1,7 @@
-package pl.com.bottega.cinemasystem.api;
+package pl.com.bottega.cinemasystem.domain;
 
 import org.springframework.stereotype.Component;
+import pl.com.bottega.cinemasystem.api.CreateShowsRequest;
 import pl.com.bottega.cinemasystem.domain.*;
 
 import java.time.LocalDateTime;
@@ -11,11 +12,10 @@ import java.util.List;
 @Component
 public class ShowsFactory {
 
-    public Collection<Show> getShows(Cinema cinema, Movie movie, CreateShowsRequest request) {
+    public static Collection<Show> createShows(Cinema cinema, Movie movie, CreateShowsRequest request) {
         Collection<LocalDateTime> dates = request.getShowDates();
         List<Show> shows = new ArrayList<>();
         dates.forEach(date -> shows.add(new Show(cinema, movie, date)));
         return shows;
     }
-
 }
