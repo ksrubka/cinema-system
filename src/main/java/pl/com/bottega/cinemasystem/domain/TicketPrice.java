ea42bb49f6805cae2a2ddef3e346c07537b8d145
ksrubka <beata.choluj@wp.pl>
2016-09-21 15:41:29 +0200

    Add fields and constructor to TicketPrice

--- src/main/java/pl/com/bottega/cinemasystem/domain/TicketPrice.java
+++ src/main/java/pl/com/bottega/cinemasystem/domain/TicketPrice.java
@@ -1,4 +1,26 @@
 package pl.com.bottega.cinemasystem.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
 public class TicketPrice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    private String type;
+    private BigDecimal price;
+
+    public TicketPrice() {
+    }
+
+    public TicketPrice(String type, BigDecimal price) {
+        this.type = type;
+        this.price = price;
+    }
 }
