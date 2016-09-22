eb3789ebfbd27793bb6734c96911fafd24d8fc30
paulina.pislewicz <paulina.pislewicz@gmail.com>
2016-09-22 21:07:55 +0200

    add listAllCinemasTest

--- src/test/java/pl/com/bottega/cinemasystem/infrastructure/JPACinemaCatalogTest.java
+++ src/test/java/pl/com/bottega/cinemasystem/infrastructure/JPACinemaCatalogTest.java
@@ -2,8 +2,6 @@ package pl.com.bottega.cinemasystem.infrastructure;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
@@ -12,6 +10,10 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import pl.com.bottega.cinemasystem.api.CinemaCatalog;
 import pl.com.bottega.cinemasystem.api.ListAllCinemasResponse;
+import pl.com.bottega.cinemasystem.domain.Cinema;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -19,16 +21,27 @@ import static org.junit.Assert.assertNotNull;
 @ContextConfiguration({"/application.xml", "/mock-auth-context.xml"})
 @TestPropertySource({"/jdbc-test.properties", "/hibernate-test.properties"})
 @WebAppConfiguration
+
 public class JPACinemaCatalogTest {
 
     @Autowired
-    CinemaCatalog cinemaCatalog;
+    CinemaCatalog jpaCinemaCatalog;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
 
     @Test
     @Transactional
     public void shouldListAllCinemas() {
         //given
-        ListAllCinemasResponse listAllCinemasResponse = cinemaCatalog.listAll();
+        Cinema cinema1 = new Cinema("Plaza", "Lublin");
+        Cinema cinema2 = new Cinema("Felicity", "Lublin");
+        entityManager.persist(cinema1);
+        entityManager.persist(cinema2);
+
+        //when
+        ListAllCinemasResponse listAllCinemasResponse = jpaCinemaCatalog.listAll();
 
         //then
         assertNotNull(listAllCinemasResponse);
