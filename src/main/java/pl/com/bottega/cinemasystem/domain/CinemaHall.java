77f9382e9d4400ae223dedb98bb62ee51a65ccd3
Nizari <nizari@o2.pl>
2016-09-25 10:58:03 +0200

    Add CinemaHall class

--- src/main/java/pl/com/bottega/cinemasystem/domain/CinemaHall.java
+++ src/main/java/pl/com/bottega/cinemasystem/domain/CinemaHall.java
@@ -1,4 +1,30 @@
 package pl.com.bottega.cinemasystem.domain;
 
+import java.util.Set;
+
 public class CinemaHall {
+
+    private char[][] tablic;
+
+    public CinemaHall(char[][] tablic) {
+        this.tablic = tablic;
+    }
+
+    public boolean checkIfSeatsCanBeReserved(Set<Seat> seats) {
+        return false; //TODO
+    }
+
+    public CinemaHall(Set<Reservation> reservations) {
+
+    }
+
+    public Set<Seat> getFreeSeats() {
+        return null; //TODO
+    }
+
+    public Set<Seat> getOccupiedSeats() {
+        return null; //TODO
+    }
+
+
 }
