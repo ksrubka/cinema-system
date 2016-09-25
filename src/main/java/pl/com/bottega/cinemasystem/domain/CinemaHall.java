d7a95728d055bc712bc2e9ffec3fd16eb5ca84e7
ksrubka <beata.choluj@wp.pl>
2016-09-25 15:53:51 +0200

    Implement generateOccupiedSeats

--- src/main/java/pl/com/bottega/cinemasystem/domain/CinemaHall.java
+++ src/main/java/pl/com/bottega/cinemasystem/domain/CinemaHall.java
@@ -1,17 +1,23 @@
 package pl.com.bottega.cinemasystem.domain;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class CinemaHall {
 
-    private boolean[][] occupiedSeats;
+    private boolean[][] seats = new boolean[10][15];
 
     public CinemaHall(Set<Reservation> reservations) {
-        this.occupiedSeats = generateOccupiedSeats(reservations);
+        this.seats = generateOccupiedSeats(reservations);
     }
 
     private boolean[][] generateOccupiedSeats(Set<Reservation> reservations) {
-        return null; //TODO
+        Set<Seat> occupiedSeatsSet = new HashSet<>();
+        reservations.forEach(reservation -> occupiedSeatsSet.addAll(reservation.getBookedSeats()));
+        for (Seat seat : occupiedSeatsSet) {
+             seats[seat.getRow()][seat.getNumber()] = true;
+        }
+        return seats;
     }
 
     public boolean canReserve(Set<Seat> seats) {
@@ -22,7 +28,7 @@ public class CinemaHall {
         return null; //TODO
     }
 
-    public Set<Seat> getOccupiedSeats() {
+    public Set<Seat> getSeats() {
         return null; //TODO
     }
 
