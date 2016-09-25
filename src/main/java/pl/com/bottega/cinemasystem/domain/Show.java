66c03410f5f0e24038cbf7be56a2a930d92acc45
Nizari <nizari@o2.pl>
2016-09-25 10:45:52 +0200

    Add new classes

--- src/main/java/pl/com/bottega/cinemasystem/domain/Show.java
+++ src/main/java/pl/com/bottega/cinemasystem/domain/Show.java
@@ -5,6 +5,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Set;
 
 @Entity
 public class Show {
