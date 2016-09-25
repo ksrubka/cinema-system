4d3890c6be140b7630e233f7769c02a31c9b94b4
ksrubka <beata.choluj@wp.pl>
2016-09-25 10:41:45 +0200

    Add loading with properties to movie and cinema repo

--- src/main/java/pl/com/bottega/cinemasystem/infrastructure/JPACinemaRepository.java
+++ src/main/java/pl/com/bottega/cinemasystem/infrastructure/JPACinemaRepository.java
@@ -21,17 +21,17 @@ public class JPACinemaRepository implements CinemaRepository {
         entityManager.persist(cinema);
     }
 
-      private void checkIfCinemaAlreadyExist(Cinema cinema) {
-            String name = cinema.getName();
-            String city = cinema.getCity();
-            List<Cinema> cinemas = entityManager.createQuery(
-                    "FROM Cinema c WHERE c.name=:name AND c.city=:city", Cinema.class)
-                    .setParameter("name", name)
-                    .setParameter("city", city)
-                    .getResultList();
-            if (!cinemas.isEmpty()) {
-                throw new InvalidRequestException("Can not persist, cinema already exists: " +
-                        name + " " + city);
+    private void checkIfCinemaAlreadyExist(Cinema cinema) {
+        String name = cinema.getName();
+        String city = cinema.getCity();
+        List<Cinema> cinemas = entityManager.createQuery(
+                "FROM Cinema c WHERE c.name=:name AND c.city=:city", Cinema.class)
+                .setParameter("name", name)
+                .setParameter("city", city)
+                .getResultList();
+        if (!cinemas.isEmpty()) {
+            throw new InvalidRequestException("Can not persist, cinema already exists: " +
+                    name + " " + city);
         }
     }
 
@@ -39,13 +39,29 @@ public class JPACinemaRepository implements CinemaRepository {
     public Cinema load(Long id) {
         try {
             return entityManager.createQuery(
-                    "SELECT DISTINCT c "+
-                    "FROM Cinema c WHERE c.id = :id",
+                    "SELECT DISTINCT c " +
+                            "FROM Cinema c WHERE c.id = :id",
                     Cinema.class)
                     .setParameter("id", id)
                     .getSingleResult();
         } catch (Exception ex) {
             throw new InvalidRequestException("No such cinema in repository: id= " + id);
+        }
+    }
+
+    @Override
+    public Cinema load(String city, String name) {
+        List<Cinema> cinemas = entityManager.createQuery(
+                "FROM Cinema c WHERE c.name=:name AND c.city=:city",
+                Cinema.class)
+                .setParameter("name", name)
+                .setParameter("city", city)
+                .getResultList();
+        if (cinemas.isEmpty()) {
+            return null;
+        }
+        else {
+            return cinemas.get(0);
         }
     }
 }
