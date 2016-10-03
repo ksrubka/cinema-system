e0dc2bcae14f3607cfc758749aaaa7b67517b6f3
ksrubka <beata.choluj@wp.pl>
2016-10-03 21:30:14 +0200

    Put cinema content into generated pdf tickets

--- src/main/java/pl/com/bottega/cinemasystem/infrastructure/PdfFacadeImpl.java
+++ src/main/java/pl/com/bottega/cinemasystem/infrastructure/PdfFacadeImpl.java
@@ -5,7 +5,7 @@ import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.stereotype.Component;
 import pl.com.bottega.cinemasystem.api.PdfFacade;
 import pl.com.bottega.cinemasystem.domain.Reservation;
-import pl.com.bottega.cinemasystem.domain.Seat;
+import pl.com.bottega.cinemasystem.domain.Ticket;
 
 import java.io.FileOutputStream;
 import java.util.Set;
@@ -17,33 +17,69 @@ public class PdfFacadeImpl implements PdfFacade {
 
     @Override
     public String createPdf(Reservation reservation) {
-        String movieTitle = reservation.getShow().getMovie().getTitle();
-        Set<Seat> seats = reservation.getBookedSeats();
-        reservation.getNumber();
-        reservation.getShow().getDate();
-        reservation.getShow().getTime();
-        reservation.getShow().getMovie().getLength();
+        Set<Ticket> tickets = TicketsFactory.create(
+                reservation.getShow(), reservation.getBookedSeats(), reservation.getTickets());
+        return generatePdfTickets(tickets, reservation);
+    }
 
-        Document document = new Document();
+    private String generatePdfTickets(Set<Ticket> tickets, Reservation reservation) {
         String path = HOME_PATH + reservation.getNumber().getNumber() + ".pdf";
+        Document document = new Document();
+        preparePdfWriter(document, path);
+        document.open();
+        prepareDocument(tickets, reservation, document);
+        document.close();
+        return path;
+    }
+
+    private void preparePdfWriter(Document document, String path) {
         try {
             PdfWriter.getInstance(document, new FileOutputStream(path));
         } catch (Exception e) {
             throw new PdfException(e);
         }
-        document.open();
-        Font chapterFont = FontFactory.getFont(FontFactory.HELVETICA, 16, Font.BOLDITALIC);
+    }
+
+    private void prepareDocument(Set<Ticket> tickets, Reservation reservation, Document document) {
         Font paragraphFont = FontFactory.getFont(FontFactory.HELVETICA, 12, Font.NORMAL);
-        Chunk chunk = new Chunk("This is the title", chapterFont);
-        Chapter chapter = new Chapter(new Paragraph(chunk), 1);
-        chapter.setNumberDepth(0);
-        chapter.add(new Paragraph("This is the paragraph", paragraphFont));
-        try {
-            document.add(chapter);
-        } catch (DocumentException e) {
-            throw new PdfException(e);
+        int chapterNumber = 1;
+        for (Ticket ticket : tickets) {
+            Chapter chapter = new Chapter(new Paragraph(""), chapterNumber);
+            chapter.add(new Paragraph("***********************************************", paragraphFont));
+            chapter.add(new Paragraph(ticket.getShow().getCinema().getCity() + " " +
+                    ticket.getShow().getCinema().getName()));
+            chapter.setNumberDepth(0);
+            chapter.add(new Paragraph(ticket.getShow().getMovie().getTitle(), paragraphFont));
+            chapter.add(new Paragraph(
+                    "Date: " +
+                            ticket.getShow().getDate().toString(), paragraphFont));
+            chapter.add(new Paragraph(
+                    "Time: " +
+                            ticket.getShow().getTime().toString(), paragraphFont));
+            chapter.add(new Paragraph(
+                    "Duration: " +
+                            ticket.getShow().getMovie().getLength().toString() + " minutes", paragraphFont));
+            chapter.add(new Paragraph(
+                    "Row: " +
+                            ticket.getSeat().getRow(), paragraphFont));
+            chapter.add(new Paragraph(
+                    "Seat number: " +
+                            ticket.getSeat().getNumber(), paragraphFont));
+            chapter.add(new Paragraph(
+                    "Ticket kind: " +
+                            ticket.getTicketPrice().getType(), paragraphFont));
+            chapter.add(new Paragraph(
+                    "Reservation number: " +
+                            reservation.getNumber().getNumber(), paragraphFont));
+            chapter.add(new Paragraph("***********************************************", paragraphFont));
+            chapter.add(new Paragraph(""));
+
+            try {
+                document.add(chapter);
+            } catch (DocumentException e) {
+                throw new PdfException(e);
+            }
+            chapterNumber++;
         }
-        document.close();
-        return path;
     }
 }
